f22032c6b964ebbcd30286a898b31428f0e0d2f6
vboxuser <vboxuser@W11>
2024-02-04 19:27:56 +0100

    Bienvenida GIT

--- src/raul/git/RaulGIT.java
+++ src/raul/git/RaulGIT.java
@@ -15,7 +15,8 @@ public class RaulGIT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola, mundo");
+        System.out.println("Hola, mundo.");
+        System.out.println("Bienvenido al proyecto para GIT del alumno Raúl Agradano González.");
     }
     
 }
