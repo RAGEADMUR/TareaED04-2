bb562e4d1596c34c6a740873e202634ea1a354ed
vboxuser <vboxuser@W11>
2024-02-04 19:49:57 +0100

    RAMA1

--- src/raul/git/RaulGIT.java
+++ src/raul/git/RaulGIT.java
@@ -17,6 +17,7 @@ public class RaulGIT {
         // TODO code application logic here
         System.out.println("Hola, mundo.");
         System.out.println("Bienvenido al proyecto para GIT del alumno Raúl Agradano González.");
+        System.out.println("Ésta es la RAMA1.");
     }
     
 }
